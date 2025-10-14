d2682ff8520c76d5692eba70d18cc7995c63e2ff
Sentsuki <52487960+Sentsuki@users.noreply.github.com>
2025-10-14 18:34:29 +0800

    fix

--- src/components/Overview.vue
+++ src/components/Overview.vue
@@ -70,18 +70,11 @@ function stopService() {
 
       <!-- 配置信息卡片 -->
       <div v-if="selectedConfigDisplay" class="overview-config-card">
-        <div class="config-content">
-          <div class="config-icon-wrapper">
-            <svg class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20">
-              <path fill-rule="evenodd" d="M4 4a2 2 0 012-2h4.586A2 2 0 0112 2.586L15.414 6A2 2 0 0116 7.414V16a2 2 0 01-2 2H6a2 2 0 01-2-2V4zm2 6a1 1 0 011-1h6a1 1 0 110 2H7a1 1 0 01-1-1zm1 3a1 1 0 100 2h6a1 1 0 100-2H7z" clip-rule="evenodd" />
-            </svg>
-          </div>
-          <div class="config-info">
-            <span class="config-name">{{ selectedConfigDisplay }}</span>
-            <span class="config-type" :class="{ subscription: isSubscription }">
-              {{ isSubscription ? 'Subscription' : 'Local File' }}
-            </span>
-          </div>
+        <div class="config-simple">
+          <span class="config-name">{{ selectedConfigDisplay }}</span>
+          <span class="config-type" :class="{ subscription: isSubscription }">
+            {{ isSubscription ? 'Subscription' : 'Local File' }}
+          </span>
         </div>
       </div>
 
